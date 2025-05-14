cd9892315a9472e844f79537d9173202f2858807
MD RAKIB HOSSAIN <117323310+hossam76@users.noreply.github.com>
2025-05-14 02:37:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1162,5 +1162,5 @@ def api_stats():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+    #app.run(debug=True)
